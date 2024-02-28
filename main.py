1581fa0974b58ff3c20e0e53be09a0102e258740
aperf3ct <158367984+aperf3ct@users.noreply.github.com>
2024-02-29 12:13:56 +1300

    school_list random

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-def gst_calculator():
-    item = int(input("Enter the price of your item: "))
-    with_gst = item * 1.15
-    print("Your item with GST is ${}".format(with_gst))
+import random
+student_list = ['Katherine', 'Nicole', 'Shreya', 'April',
+                'Anya', 'Abby', 'Melissa', 'Kaitlyn', 'Michelle', 'Olivia', 'Mr Harrison']
+valid = False
+choice_1 = 0
+choice_2 = 0
 
+while valid == False:
+    choice_1 = random.choice(student_list)
+    choice_2 = random.choice(student_list)
+    if choice_1 == choice_2:
+        choice_2 = random.choice(student_list)
+    else:
+        valid = True
 
-gst_calculator()
+print("{} vs {}".format(choice_1, choice_2))
